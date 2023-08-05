bf253dff91da4189fd8d36a2e8cb2f4e6f4bf726
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-08-04 21:22:30 -0300

    Update TextureUtil_freeBufferMixin.java

--- common/src/main/java/ca/fxco/memoryleakfix/mixin/readResourcesLeak/TextureUtil_freeBufferMixin.java
+++ common/src/main/java/ca/fxco/memoryleakfix/mixin/readResourcesLeak/TextureUtil_freeBufferMixin.java
@@ -1,25 +1,17 @@
 package ca.fxco.memoryleakfix.mixin.readResourcesLeak;
 
-import ca.fxco.memoryleakfix.config.*;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import org.lwjgl.system.MemoryUtil;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Coerce;
 
 import java.nio.ByteBuffer;
 import java.nio.channels.Channel;
 
-// class_4536 is the fabric intermediary class name of TextureUtil in 1.15.x,
-// net/minecraft/client/texture/TextureUtil is the fabric mapping to make it work in dev
-// where the class was part of the net.minecraft package, method_24962 (Fabric) / func_225684_a_ (Forge) are the equivalents of readResource
-//@MinecraftRequirement(@VersionRange(minVersion = "1.15.0", maxVersion = "1.16.5"))
-@Pseudo
-@SilentClassNotFound
 @Environment(EnvType.CLIENT)
 @Mixin(targets = {
         "com/mojang/blaze3d/platform/TextureUtil",
@@ -39,10 +31,6 @@ public abstract class TextureUtil_freeBufferMixin {
      */
 
     @SuppressWarnings("MixinAnnotationTarget")
-    @Remaps({
-            @Remap(fabric = "method_24962", excludeDev = true, mcVersions = @MinecraftRequirement(@VersionRange(minVersion = "1.16.0", maxVersion = "1.16.5"))),
-            @Remap(mcp = "func_225684_a_", excludeDev = true)
-    })
     @WrapOperation(
             method = "readResource(Ljava/io/InputStream;)Ljava/nio/ByteBuffer;",
             at = {
@@ -56,12 +44,12 @@ public abstract class TextureUtil_freeBufferMixin {
                     )
             }
     )
-    private static int memoryLeakFix$readResourceWithoutLeak(@Coerce Channel channel, ByteBuffer byteBuf,
-                                                             Operation<Integer> original) {
+    private static int fixMemoryLeakOnReadResource(@Coerce Channel channel, ByteBuffer byteBuffer,
+                                                   Operation<Integer> original) {
         try {
-            return original.call(channel, byteBuf);
+            return original.call(channel, byteBuffer);
         } catch (Exception e) {
-            MemoryUtil.memFree(byteBuf);
+            MemoryUtil.memFree(byteBuffer);
             throw e;
         }
     }
