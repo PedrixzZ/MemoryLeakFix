c2127ce59a30d47bbe4981103a3e4c6750d83b3e
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-08-04 21:14:35 -0300

    Update LivingEntity_clearMemoriesMixin.java

--- common/src/main/java/ca/fxco/memoryleakfix/mixin/entityMemoriesLeak/LivingEntity_clearMemoriesMixin.java
+++ common/src/main/java/ca/fxco/memoryleakfix/mixin/entityMemoriesLeak/LivingEntity_clearMemoriesMixin.java
@@ -9,19 +9,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/*
- * Fixes https://bugs.mojang.com/browse/MC-260605
- * This was fixed by mojang in 1.19.4
- */
-
-@MinecraftRequirement(@VersionRange(maxVersion = "1.19.3"))
+@MinecraftRequirement(@VersionRange(maxVersion = "1.20.1"))
 @Mixin(LivingEntity.class)
 public abstract class LivingEntity_clearMemoriesMixin extends Entity_clearMemoriesMixin {
 
-    @Shadow protected Brain<?> brain;
+    @Shadow
+    protected Brain<?> brain;
 
     @Override
     protected void memoryLeakFix$OnEntityRemoved(CallbackInfo ci) {
-        ((ExtendBrain)this.brain).memoryLeakFix$clearMemories();
+        ((ExtendBrain) this.brain).memoryLeakFix$clearMemories();
     }
 }
